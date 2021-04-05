fdc2284102b5eeac76b43ce5dfbb4a9ce3ac7348
Luo <luoyuhua1996@vip.qq.com>
2021-04-05 23:08:55 +0800

    Initial commit

---
+++ src/main/java/com/knluo/elgamalattack/ElgamalattackApplication.java
@@ -0,0 +1,13 @@
+package com.knluo.elgamalattack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElgamalattackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElgamalattackApplication.class, args);
+    }
+
+}
